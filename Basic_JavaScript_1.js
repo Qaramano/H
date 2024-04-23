7fe28f7ddc2d80429edbacc5e6966fcca07181da
Qaramano <omarqaraman@hotmail.com>
2024-04-22 21:14:06 -0600

    Added Basic_HTMl_CSS_and_JavaScript_1 folder

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+alert("Hello, My name is Omar!");
+
+
+	
+	function My_First_Function(){
+		var newtext = "Salute me, I'm Palestinian";
+		var result = newtext.fontcolor("blue");
+		document.getElementById("Palestinian").innerHTML =
+		result;
+		}
