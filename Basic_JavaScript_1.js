3b4e3f2cf3850afd23b0ef6f3c3ddb711f016665
Qaramano <omarqaraman@hotmail.com>
2024-04-23 22:20:50 -0600

    Added table and list with style file

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -5,6 +5,5 @@ alert("Hello, My name is Omar!");
 	function My_First_Function(){
 		var newtext = "Salute me, I'm Palestinian";
 		var result = newtext.fontcolor("blue");
-		document.getElementById("Palestinian").innerHTML =
-		result;
+		document.getElementById("Palestinian").innerHTML =result;
 		}
